b37d20aad21109605c74e642fa97fa650ed24ce0
Mikaz-fr <mikaz.roy@gmail.com>
2012-12-05 15:00:37 +0100

    Few fix for pipeline and first synthesis. Results are different from simple sim, rethink is needed

--- SRC/components/array_t.vhd
+++ SRC/components/array_t.vhd
@@ -25,27 +25,28 @@ PACKAGE array_t IS
 	constant A_WIDTH: natural := 24;
 	constant B_WIDTH: natural := 23;
 	constant NLEVEL : natural := 8; 	--Pipeline included
-	constant ELEM_ADDER : natrual := 0;
-	constant ELEM_REG : natrual := 1;
+	constant ELEM_ADDER : natural := 0;
+	constant ELEM_REG : natural := 1;
 	TYPE net_mat is array (natural range <>) of array32_t(0 to A_WIDTH-1);	--matrix of wire, each entry has maximum 3 entries and there is NIN entries
 	TYPE vect5 is array (0 to 4) of natural;	--number of element, then shifts numbers
-	TYPE vect2 is array (0 to 1) of natural
-	TYPE matrix_vect is array (natural range <>) of vect5;
-	constant a_shift_arr: matrix_vect(0 to (NIN/2)-1) := (
+	TYPE vect2 is array (0 to 1) of natural;
+	TYPE matrix_vect2 is array (natural range <>) of vect2;
+	TYPE matrix_vect5 is array (natural range <>) of vect5;
+	constant a_shift_arr: matrix_vect5(0 to (NIN/2)-1) := (
 		(2,1,0,0,0),
 		(3,4,3,0,2),
 		(2,6,5,0,5),
 		(2,8,7,0,7),
 		(3,11,10,0,9)
 		);
-	constant b_shift_arr: matrix_vect(0 to (NIN/2)-1) := (
+	constant b_shift_arr: matrix_vect5(0 to (NIN/2)-1) := (
 		(2,2,0,0,0),
 		(2,5,4,0,2),
 		(3,6,3,2,4),
 		(3,8,6,0,7),
 		(3,10,9,2,10)
 		);
-	constant a_elem_index: vect2(0 to NLEVEL-2) := (
+	constant a_elem_index: matrix_vect2(0 to NLEVEL-2) := (
 		(12, 0),
 		(6, 0),
 		(0, 6),
@@ -55,7 +56,7 @@ PACKAGE array_t IS
 		(1, 0)
 	   );		
 
-	constant b_elem_index: vect2(0 to NLEVEL-2) := (
+	constant b_elem_index: matrix_vect2(0 to NLEVEL-2) := (
 		(11, 0),
 		(6, 0),
 		(0, 6),
